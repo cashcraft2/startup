47732771331b82feb6a1db5963368757537dc315
cashcraft2 <cashcraft2@gmail.com>
2025-10-06 16:44:04 -0600

    Create the router

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Signin } from './signin/signin';
+import { Home } from './home/home';
+import { Log } from './log/log';
+import { Plan } from './plan/plan';
 
 export default function App() {
-  return (
-    <div>
-        <header>
-            <nav>
-                <menu className="navbar-nav">
-                    <a className="nav-sign-in" href="index.html">Sign In</a>
-                </menu>
-            </nav>
-        </header>
+    return (
+      <BrowserRouter>
+        <div>
+            <header>
+                <nav>
+                    <menu className="navbar-nav">
+                        <NavLink className="nav-sign-in" to="signin">Sign In</NavLink>
+                    </menu>
+                </nav>
+            </header>
 
-        <main> App components go here</main>
+            <Routes>
+                <Route path='/' element={<Signin />} exact />
+                <Route path='/home' element={<Home />} />
+                <Route path='/log' element={<Log />} />
+                <Route path='/plan' element={<Plan />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-        <footer>
-            <div>
-                <span className="author"> Author: </span>
-                <a href="https://github.com/cashcraft2/startup">Cotter Ashcraft</a>
-            </div>
-        </footer>
-    </div>
-  );
-}
+            <footer>
+                <div>
+                    <span className="author"> Author: </span>
+                    <NavLink to="https://github.com/cashcraft2/startup">Cotter Ashcraft</NavLink>
+                </div>
+            </footer>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
+  function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
---
+++ src/plan/plan.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Plan() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>plan displayed here</div>
+    </main>
+  );
+}
