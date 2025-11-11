6e258cd8d5565ae9b706f36d870ddb5811f7ac6f
cashcraft2 <cashcraft2@gmail.com>
2025-11-10 23:12:22 -0700

    Initial MongoDB setup

---
+++ service/database.js
@@ -0,0 +1,8 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('outfishn');
+const userCollection = db.collection('users');
+const scoreCollection = db.collection('catches');
