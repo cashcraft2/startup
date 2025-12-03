224c73d8170161f8b27915191029a300a0e261a8
cashcraft2 <cashcraft2@gmail.com>
2025-12-02 21:13:40 -0700

    Add testing functionality by modifying vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()], 
   server: {
-  
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
+      '/api': 'http://localhost:4000',
+      '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true,
       },
     },
   },
-})
+});
